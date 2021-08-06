e86bcb0354b3fe7125d4ce02348e0ae7534aa170
blindbox <blindcan@gmail.com>
2021-08-06 22:55:44 +0800

    UNTESTED: Finish the challenge

--- leetcode.com/1689/typescript/src/challenge.ts
+++ leetcode.com/1689/typescript/src/challenge.ts
@@ -1,3 +1,30 @@
+/**
+ * https://leetcode.com/problems/partitioning-into-minimum-number-of-deci-binary-numbers/
+ * @param n
+ */
 export function minPartitions(n: string): number {
-    return 0
+    // idea: we try to subtract as frequently as possible with the largest possible deci-binary.
+
+    const getLargestDeciBinary = (input: string) => {
+        let deciBinary = ''
+
+        input.split('').forEach(letter => {
+            if (Number(letter)) {
+                deciBinary += '1'
+            } else {
+                deciBinary += '0'
+            }
+        })
+    }
+
+    let accumulator = Number(n)
+    let timesSubtracted = 0
+
+    while (accumulator !== 0) {
+        const deciBinary = Number(getLargestDeciBinary(String(accumulator)))
+        accumulator -= deciBinary
+        timesSubtracted++
+    }
+
+    return timesSubtracted
 }
